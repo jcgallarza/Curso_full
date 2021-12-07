25aeb34ddebcdfd3cdd36a94edbf3c23a9b090e5
Juan Gallarza <juan.gallarza@hotmail.conm>
2021-12-07 12:26:52 -0600

    Handling error

--- Curso_full/ViewController.swift
+++ Curso_full/ViewController.swift
@@ -12,5 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Error")
+    }
+    
 }
 
